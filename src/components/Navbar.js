2f9bc7975b25756a675b7300b836318a5e8d91db
rezahakimalzami03 <rezahakimalzami03@gmail.com>
2024-06-17 20:54:57 +0700

    update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -208,7 +208,7 @@ function Navbar() {
                     aria-labelledby="dropdownEtalaseNavbarLink">
                     <li>
                       <a
-                        href="/merchendise"
+                        href="/merchandise"
                         className="camelcase block px-4 py-2 hover:bg-gray-100 hover:bg-gray-400 hover:text-black">
                         Merchandise
                       </a>
